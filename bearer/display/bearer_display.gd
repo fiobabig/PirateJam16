7ed288f4601f8d6124be39274b5b76ba1d5cbd24
Brian <upta@outlook.com>
2025-01-22 18:51:28 -0800

    Updated alignment calculation to be based on inclination

--- bearer/display/bearer_display.gd
+++ bearer/display/bearer_display.gd
@@ -1,7 +1,10 @@
 extends MarginContainer
 
 @onready var name_label: Label = %Name
-@onready var alignment_label: Label = %Alignment
+@onready var bravery_inclination_label: Label = %BraveryInclination
+@onready var compassion_inclination_label: Label = %CompassionInclination
+@onready var justice_inclination_label: Label = %JusticeInclination
+@onready var temperance_inclination_label: Label = %TemperanceInclination
 @onready var bond_label: Label = %Bond
 @onready var bravery_label: Label = %Bravery
 @onready var compassion_label: Label = %Compassion
@@ -27,13 +30,16 @@ func _on_score_changed(score: float):
 func _on_bearer_changed(bearer: BearerResource):
 	visible = true
 	name_label.text = bearer.name
-	alignment_label.text = "Alignment: " + str(bearer.alignment)
 	bond_label.text = "Bond: " + str(bearer.bond)
 	bravery_label.text = "Bravery: " + str(bearer.bravery)
 	compassion_label.text = "Compassion: " + str(bearer.compassion)
 	justice_label.text = "Justice: " + str(bearer.justice)
 	temperance_label.text = "Temperance: " + str(bearer.temperance)
 	lifespan_label.text = "Lifespan: " + str(bearer.lifespan)
+	bravery_inclination_label.text = "Bravery Inclination: " + str(bearer.bravery_inclination)
+	compassion_inclination_label.text = "Compassion Inclination: " + str(bearer.compassion_inclination)
+	justice_inclination_label.text = "Justice Inclination: " + str(bearer.justice_inclination)
+	temperance_inclination_label.text = "Temperance Inclination: " + str(bearer.temperance_inclination)
 
 
 func _on_selected_changed(skills: Array[Enums.Skills]):
--- bearer/service/bearer_service.gd
+++ bearer/service/bearer_service.gd
@@ -17,12 +17,20 @@ func create() -> BearerResource:
 
 	_current = BearerResource.new()
 	_current.name = first_name + " " + last_name
-	_current.alignment = _random_stat()
+
+	# hit points
 	_current.bond = 0
+
+	# alignment
+	_add_inclination(_current)
+
+	# stats modified by decisions (inflection stats)
 	_current.bravery = _random_stat()
 	_current.compassion = _random_stat()
 	_current.justice = _random_stat()
 	_current.temperance = _random_stat()
+
+	# longevity
 	_current.lifespan = 3
 
 	changed.emit(_current)
@@ -50,10 +58,12 @@ func update(option: DecisionOption):
 	_current.justice = _cap(_current.justice + option.justice)
 	_current.temperance = _cap(_current.temperance + option.temperance)
 
-	if sign(_current.alignment) == sign(option.alignment):
-		_current.bond += abs(option.alignment)
-	else:
-		_current.bond -= abs(option.alignment)
+	_current.bond += (
+		(option.bravery * _current.bravery_inclination)
+		+ (option.compassion * _current.compassion_inclination)
+		+ (option.justice * _current.justice_inclination)
+		+ (option.temperance * _current.temperance_inclination)
+	)
 
 	_current.bond = _cap(_current.bond)
 
@@ -71,3 +81,39 @@ func _cap(value: int):
 
 func _random_stat():
 	return randi() % 201 - 100
+
+
+func _add_inclination(bearer: BearerResource):
+	bearer.bravery_inclination = 0.0
+	bearer.compassion_inclination = 0.0
+	bearer.justice_inclination = 0.0
+	bearer.temperance_inclination = 0.0
+
+	var primary_inclination_stat = randi_range(1, 4)
+	var secondary_inclination_stat = randi_range(1, 4)
+
+	var primary_inclination_value = -1.0 if randi_range(0, 1) == 0.0 else 1.0
+	var secondary_inclination_value = 0.5 * (sign(primary_inclination_value) * -1)
+
+	while secondary_inclination_stat == primary_inclination_stat:
+		secondary_inclination_stat = randi_range(1, 4)
+
+	match primary_inclination_stat:
+		1:
+			bearer.bravery_inclination = primary_inclination_value
+		2:
+			bearer.compassion_inclination = primary_inclination_value
+		3:
+			bearer.justice_inclination = primary_inclination_value
+		4:
+			bearer.temperance_inclination = primary_inclination_value
+
+	match secondary_inclination_stat:
+		1:
+			bearer.bravery_inclination = secondary_inclination_value
+		2:
+			bearer.compassion_inclination = secondary_inclination_value
+		3:
+			bearer.justice_inclination = secondary_inclination_value
+		4:
+			bearer.temperance_inclination = secondary_inclination_value
