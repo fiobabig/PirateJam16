5fa6d4f9f3c8750f23d751afe56f5178077bf388
Spencer Tyler <Stylercode@gmail.com>
2025-01-24 22:26:29 -0800

    Main Menu, Settings, and Credits

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,8 +1,12 @@
 extends Control
+@onready var settings: Control = %Settings
+@onready var credits: Control = %Credits
 
 
 func _ready() -> void:
 	GameService.started.connect(_on_started)
+	settings.visible = false
+	credits.visible = false
 
 
 func _on_start_pressed() -> void:
@@ -11,3 +15,23 @@ func _on_start_pressed() -> void:
 
 func _on_started():
 	visible = false
+
+
+func _on_settings_pressed() -> void:
+	settings.visible = true
+
+
+func _on_close_settings_pressed() -> void:
+	settings.visible = false
+
+
+func _on_credits_pressed() -> void:
+	credits.visible = true
+
+
+func _on_close_credits_pressed() -> void:
+	credits.visible = false
+
+
+func _on_audio_volume_value_changed(value: float) -> void:
+	pass  # Replace with function body.
