788e64fc97e2f527f84de9aeff37a6813ae0d565
Brian <upta@outlook.com>
2025-01-25 20:29:03 -0800

    Victory UI and ability to go back to main menu

--- app/game.gd
+++ app/game.gd
@@ -9,6 +9,15 @@ func _ready() -> void:
 	main_menu.visible = true
 
 	GameService.started.connect(_on_started)
+	GameService.go_to_main_menu.connect(_on_go_to_main_menu)
+
+
+func _on_go_to_main_menu():
+	BearerService.reset()
+	EventService.reset()
+
+	game_ui.visible = false
+	main_menu.visible = true
 
 
 func _on_started():
