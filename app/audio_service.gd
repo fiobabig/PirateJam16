8e2636815e7a7327911cb764b2abb2ada12399f0
Spencer Tyler <Stylercode@gmail.com>
2025-01-26 17:05:43 -0800

    Music & Sound Effects

--- app/audio_service.gd
+++ app/audio_service.gd
@@ -1,3 +1,18 @@
 extends Node
 
 @onready var game_music: AudioStreamPlayer = %GameMusic
+@onready var game_start_sfx: AudioStreamPlayer = %GameStartSFX
+@onready var button_select: AudioStreamPlayer = %ButtonSelect
+@onready var write_decision: AudioStreamPlayer = %WriteDecision
+@onready var change_decision: AudioStreamPlayer = %ChangeDecision
+@onready var bond_broken: AudioStreamPlayer = %BondBroken
+@onready var bearer_died: AudioStreamPlayer = %BearerDied
+@onready var victory: AudioStreamPlayer = %Victory
+
+
+func _on_change_decision_finished() -> void:
+	AudioService.write_decision.play()
+
+
+func _on_button_select_finished() -> void:
+	AudioService.change_decision.play()
